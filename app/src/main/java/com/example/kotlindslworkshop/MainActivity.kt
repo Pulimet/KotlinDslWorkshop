428980ddedc21a65ac158d2674618d639b42267c
Pulimet <korolev2009@gmail.com>
2020-03-29 06:51:09 +0300

    MainActivity - Starting point

--- app/src/main/java/com/example/kotlindslworkshop/MainActivity.kt
+++ app/src/main/java/com/example/kotlindslworkshop/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.kotlindslworkshop
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sqlString = SqlBuilder().build()
+        tvResult.text = sqlString
     }
+}
+
+class SqlBuilder {
+    fun build() = "select * from my_table"
 }
