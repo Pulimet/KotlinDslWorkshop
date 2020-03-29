7dcb34130eb219389e3c73ce049b85ef0eadc4c6
Pulimet <korolev2009@gmail.com>
2020-03-29 08:38:03 +0300

    MainActivity - Added to function "query" a parameter of type function and invoked it inside the body function. When calling a "query" function passed as an argument a lambda that prints a log.

--- app/src/main/java/com/example/kotlindslworkshop/MainActivity.kt
+++ app/src/main/java/com/example/kotlindslworkshop/MainActivity.kt
@@ -1,24 +1,29 @@
 package com.example.kotlindslworkshop
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val tag = "WORKSHOP_TAG"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvResult.text = query().build()
+        tvResult.text = query {
+            Log.d(tag, "Lambda called")
+        }.build()
     }
 }
 
-fun query() : SqlBuilder {
+fun query(init: () -> Unit) : SqlBuilder {
+    init()
     return SqlBuilder()
 }
 
 class SqlBuilder {
     fun build() = "select * from my_table"
 }
-
